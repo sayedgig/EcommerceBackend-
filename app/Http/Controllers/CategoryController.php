2a4f71348d1eff6476d84e3ebdd33b12e20a793d
sayedgig <sayedgig@gmail.com>
2022-02-03 07:56:35 +0300

    03022022 part 9
    03022022 part 9

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class CategoryController extends Controller
+{
+    
+
+    function store(Request $request){
+
+        $validator = Validator::make($request->all(),[
+            'name' => 'required',
+            'slug' => 'required',
+            'meta_title' => 'required',
+        ]);
+    
+        if ($validator->fails()){
+            return response()->json([
+                'status'=>400,
+                'errors'=>$validator->messages(),
+            ]);
+        }
+        else{
+            $category                    = new Category;
+            $category->slug              = $request->input('slug');
+            $category->name              = $request->input('name');
+            $category->description        = $request->input('description');
+            $category->status            = $request->input('status');
+            $category->meta_title        = $request->input('meta_title');
+            $category->meta_description  = $request->input('meta_description');
+            $category->meta_keyword      = $request->input('meta_keyword');
+            $category->save();
+            return response()->json([
+                'status'=>200,
+                'message'=>'category added sucessfully'
+            ]);
+        }
+
+        
+
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use  App\Http\Controllers\BookController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -25,6 +27,8 @@ Route::middleware('auth:sanctum')->get('/book', [BookController::class,'index'])
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('logout',[AuthController::class,'logout']);
     Route::get('checkAuth',[AuthController::class,'isApiAdmin']);
+
+    Route::post('store-category',[CategoryController::class,'store']);
 });
 
 // Route::middleware('auth:sanctum','isApiAdmin')->group(function () {
